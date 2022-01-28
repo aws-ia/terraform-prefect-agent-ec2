d379afd165a561027efd338c14100df82626d1a6
Jamie Dick <jamie@prefect.io>
2022-01-28 18:08:25 -0500

    remove awscc

--- versions.tf
+++ versions.tf
@@ -5,17 +5,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.72.0"
     }
-    awscc = {
-      source  = "hashicorp/awscc"
-      version = ">= 0.9.0"
-    }
   }
-}
-
-provider "awscc" {
-  user_agent = [{
-    product_name    = "terraform-awscc-"
-    product_version = "0.0.1"
-    comment         = "V1/AWS-D69B4015/<github repo id>"
-  }]
 }
