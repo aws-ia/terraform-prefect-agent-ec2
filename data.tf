c0b438cc297792d58a0445f86f50b5476006ecf1
Jamie Dick <jamie@prefect.io>
2022-02-02 17:44:23 -0500

    dynamic ami pull

--- data.tf
+++ data.tf
@@ -2,6 +2,20 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+}
+
 data "aws_secretsmanager_secret" "prefect" {
   name = var.prefect_api_key_secret_name
 }
